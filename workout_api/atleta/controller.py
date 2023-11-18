dd1aaee235ab471660cd838ecd2fb662fbbd1c2d
Rafael Ortiz <rhb.ortiz@gmail.com>
2023-11-18 10:05:09 -0300

    Adds search by id, cpf and name to the athletes endpoint

--- workout_api/atleta/controller.py
+++ workout_api/atleta/controller.py
@@ -1,11 +1,12 @@
 from uuid import uuid4
 from datetime import datetime
-from fastapi import APIRouter, status, Body, HTTPException
+from fastapi import APIRouter, status, Body, HTTPException, Query
 from pydantic import UUID4
 
 from workout_api.atleta.models import AtletaModel
 from workout_api.atleta.schemas import AtletaSchemaIn, AtletaSchemaOut, AtletaSchemaPatch
 from workout_api.contrib.dependencies import DatabaseDependency
+from sqlalchemy import func
 from sqlalchemy.future import select
 from workout_api.categorias.models import CategoriaModel
 from workout_api.centro_treinamento.models import CentroTreinamentoModel
@@ -52,39 +53,34 @@ async def post(
 
 
 @router.get(
-    "/",
+    "",
     status_code=status.HTTP_200_OK,
-    summary="Lista todos os atletas",
+    summary="Lista atletas ou busca por ID, CPF ou nome",
     response_model=list[AtletaSchemaOut]
 )
 async def query(
-    db_session: DatabaseDependency
-) -> list[AtletaSchemaOut]:
-    atletas: list[AtletaSchemaOut] = (
-        await db_session.execute(select(AtletaModel))
-    ).scalars().all()
-
-    return [AtletaSchemaOut.model_validate(atleta) for atleta in atletas]
-
-
-@router.get(
-    "/{id_atleta}",
-    status_code=status.HTTP_200_OK,
-    summary="Busca um atleta por id",
-    response_model=AtletaSchemaOut
-)
-async def get(
     db_session: DatabaseDependency,
-    id_atleta: UUID4
-) -> AtletaSchemaOut:
-    atleta: AtletaSchemaOut = (
-        await db_session.execute(select(AtletaModel).filter_by(id=id_atleta))
-    ).scalars().first()
+    id_atleta: UUID4 = Query(None),
+    nome: str = Query(None),
+    cpf: str = Query(None)
+) -> list[AtletaSchemaOut]:
+    custom_query = select(AtletaModel)
 
-    if atleta is None:
+    if id_atleta:
+        custom_query = custom_query.filter_by(id=id_atleta)
+    elif cpf:
+        custom_query = custom_query.filter_by(cpf=cpf)
+    elif nome:
+        pattern = f"%{nome}%"
+        custom_query = custom_query.filter(AtletaModel.nome.ilike(pattern))
+
+    result = await db_session.execute(custom_query)
+    atletas: list[AtletaSchemaOut] = result.scalars().all()
+
+    if not atletas:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Atleta não encontrado")
 
-    return AtletaSchemaOut.model_validate(atleta)
+    return [AtletaSchemaOut.model_validate(atleta) for atleta in atletas]
 
 
 @router.patch(
